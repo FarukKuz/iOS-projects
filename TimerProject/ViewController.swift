68d7698c9a7edfa59c7f577a351057f315c664fa
FarukKuz <faruk.kuz@icloud.com>
2024-09-02 19:57:47 +0300

    Timer func used
    -

---
+++ TimerProject/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  TimerProject
+//
+//  Created by Faruk Kuz on 1.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    var timer = Timer()
+    var counter = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        counter = 10
+        timeLabel.text = "Time: \(counter)"
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFunction), userInfo: nil, repeats: true)
+    }
+    
+    @objc func timerFunction(){
+        timeLabel.text = "Time: \(counter)"
+        counter -= 1
+        
+        if counter < -1 {
+            timer.invalidate()
+            timeLabel.text = "Time's Over!"
+        }
+    }
+
+    @IBAction func buttonClicked(_ sender: Any) {
+        print ("clicked")
+    }
+    
+}
+
